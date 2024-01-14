72f754771ebda96d15fb14cb1096bb86ec1e1b50
selena-ilic <selena@ndc-media.com>
2024-01-14 01:08:09 +0100

    Set current Navbar Item to Active

--- laravel-e-commerce/resources/views/layouts/inc/sidebar.blade.php
+++ laravel-e-commerce/resources/views/layouts/inc/sidebar.blade.php
@@ -9,19 +9,19 @@
         </a></div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            <li class="nav-item active ">
+            <li class="nav-item {{ \Illuminate\Support\Facades\Request::is('dashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('dashboard') }}">
                     <i class="material-icons">dashboard</i>
                     <p>Dashboard</p>
                 </a>
             </li>
-            <li class="nav-item ">
+            <li class="nav-item {{ \Illuminate\Support\Facades\Request::is('categories') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('categories') }}">
                     <i class="material-icons">person</i>
                     <p>Categories</p>
                 </a>
             </li>
-            <li class="nav-item ">
+            <li class="nav-item {{ \Illuminate\Support\Facades\Request::is('add-category') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('add-category') }}">
                     <i class="material-icons">content_paste</i>
                     <p>Add Category</p>
